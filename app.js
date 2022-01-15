06442e6fbb059f444dd1b96bbfb7958d254e75aa
fishworm96 <245525860@qq.com>
2022-01-15 15:18:05 +0800

    feat: 路由自动加载

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const Koa = require('koa')
 const Router = require('koa-router')
+const requireDirectory = require('require-directory')
+
 const router = new Router()
 const app = new Koa()
 
-app.use(async (ctx, next) => {
-  ctx.body = 'hello world'
-})
+const modules = requireDirectory(module, './api/v1', { visit: whenLoadModule })
+
+function whenLoadModule (obj) {
+  if (obj instanceof Router) {
+    app.use(obj.routes())
+  }
+}
+
 
 app.listen(3000)
